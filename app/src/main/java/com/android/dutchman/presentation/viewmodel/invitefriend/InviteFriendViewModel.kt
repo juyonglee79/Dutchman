a85ba085cdd72c62c64b13835196c323788edb8c
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-04-06 11:49:11 +0900

    [UPDATE]InviteFriendDialogFragment viewmodel 추가

--- app/src/main/java/com/android/dutchman/presentation/viewmodel/invitefriend/InviteFriendViewModel.kt
+++ app/src/main/java/com/android/dutchman/presentation/viewmodel/invitefriend/InviteFriendViewModel.kt
@@ -1,15 +1,24 @@
 package com.android.dutchman.presentation.viewmodel.invitefriend
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.android.dutchman.domain.repository.invitefriend.InviteFriendRepository
 import com.android.dutchman.util.SingleLiveEvent
 
 class InviteFriendViewModel(val inviteFriendRepository: InviteFriendRepository) : ViewModel() {
 
+    var btnColorSet : Boolean = true
+
     val showMyFriendsLiveEvent = SingleLiveEvent<Any>()
     val inviteFriendsLiveEvent = SingleLiveEvent<Any>()
 
-    fun showMyFriends() = showMyFriendsLiveEvent.call()
-    fun inviteFriends() = inviteFriendsLiveEvent.call()
+    fun showMyFriends() {
+        btnColorSet = true
+        showMyFriendsLiveEvent.call()
+    }
 
+    fun inviteFriends() {
+        btnColorSet = false
+        inviteFriendsLiveEvent.call()
+    }
 }
